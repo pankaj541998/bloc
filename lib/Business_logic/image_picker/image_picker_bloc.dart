ef385a962e5f5f22480aa53be7f6ae28844e2cab
Pankaj <pankaj@wdimails.com>
2024-04-07 07:36:25 +0530

    worknig on image picker

--- lib/Business_logic/image_picker/image_picker_bloc.dart
+++ lib/Business_logic/image_picker/image_picker_bloc.dart
@@ -1,5 +1,13 @@
 
+import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:image_picker/image_picker.dart';
 
 part "image_picker_events.dart";
 part "image_picker_state.dart";
+
+
+class  ImagePickerBloc extends Bloc<ImagePickerEvents, ImagePickerState> {
+  ImagePickerBloc(super.initialState);
+  
+}
